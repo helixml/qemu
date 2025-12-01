6e32909fd85e9acca94372c2a448fb2d7d7b1520
Joelle van Dyne <j@getutm.app>
2025-11-30 16:43:17 -0800

    cocoa: support scanout of Metal textures
    We still require GLES support as a base for now but if Metal is available
    and virglrenderer supports native MTLTexture handles, we have a faster
    blit path.
    
    Signed-off-by: Joelle van Dyne <j@getutm.app>

--- ui/egl-helpers.c
+++ ui/egl-helpers.c
@@ -551,7 +551,32 @@ int qemu_egl_init_dpy_cocoa(DisplayGLMode mode)
         return -1;
     }
 
-    return qemu_egl_init_dpy(dpy, mode);
+    if (qemu_egl_init_dpy(dpy, mode) < 0) {
+        return -1;
+    }
+
+#ifdef EGL_METAL_DEVICE_ANGLE
+    if (epoxy_has_egl_extension(qemu_egl_display, "EGL_EXT_device_query")) {
+        EGLDeviceEXT device;
+        void *metal_device;
+
+        if (!eglQueryDisplayAttribEXT(qemu_egl_display,
+                                      EGL_DEVICE_EXT,
+                                      (EGLAttrib *)&device)) {
+            return 0;
+        }
+
+        if (!eglQueryDeviceAttribEXT(device,
+                                     EGL_METAL_DEVICE_ANGLE,
+                                     (EGLAttrib *)&metal_device)) {
+            return 0;
+        }
+
+        qemu_egl_angle_native_device = metal_device;
+    }
+#endif
+
+    return 0;
 }
 
 #if defined(CONFIG_X11) || defined(CONFIG_GBM) || defined(WIN32)
